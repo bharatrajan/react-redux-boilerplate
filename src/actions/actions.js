d509d64297689045ca425d396ea0cbeb49109501
Rajan, Bharat (Contractor) <Bharat_Rajan@comcast.com>
2018-03-05 00:27:32 -0600

    Using a live API

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,19 @@ import * as api from '../api/api';
 import actionTypes from './types';
 import _ from 'lodash';
 
-export const sampleActionDispatcher = () => ({
+export const sampleActionDispatcher = () => dispatch => 
+    api.getAllData()
+        .then(books => dispatch(sampleActionSuccessHandler(books)))
+        .catch(error => dispatch(sampleActionErrorHandler(error)));
+
+const sampleActionSuccessHandler = books => ({
     type: actionTypes.ACTION_1,
-    bp_data: api.getAllData(),
-});
+    bp_data: books,
+  });
+
+
+  const sampleActionErrorHandler = error => ({
+    type: actionTypes.ACTION_1,
+    bp_data: error,
+  });  
+
--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,17 @@
-let api = '/api';
-if(window.location.hostname.indexOf("localhost") > -1)
-  api = 'http://localhost:3001/api';
+const api = "https://reactnd-books-api.udacity1.com"
 
 // Generate a unique token for storing your bookshelf data on the backend server.
-let token = localStorage.token;
-if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
 
 const headers = {
-  Accept: 'application/json',
-  Authorization: token,
-};
+  'Accept': 'application/json',
+  'Authorization': token
+}
 
-/*
-export const getAllCategories = () =>
-  fetch(`${api}/categories`, { headers })
+export const getAllData = () =>
+  fetch(`${api}/books`, { headers })
     .then(res => res.json())
-    .then(categories => categories);
-*/
-
-
-export const getAllData = () => ({foo: "bar"})
+    .then(data => data.books)
+    .catch(error => error)    
